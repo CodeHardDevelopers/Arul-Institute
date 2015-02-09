8cce18de301e3fca5a56b551b80e7c7856b35298
Prasanth <prashanth702@gmail.com>
2015-02-08 21:12:37 -1200

    Angular update and scroll implementation

--- site/src/main/webapp/app.js
+++ site/src/main/webapp/app.js
@@ -1,1 +1,92 @@
 angular.module('app', []);
+angular.module('app').controller('HeaderController', ['$rootScope', '$timeout', function ($rootScope, $timeout) {
+    var self = this;
+    self.navigated = false;
+    self.navigators = [{
+        index: 1,
+        page: "Home",
+        highlight: false,
+        selected: true
+    },
+    {
+        index: 2,
+        page: "About",
+        highlight: false,
+        selected: false
+    },
+    {
+        index: 3,
+        page: "Course",
+        highlight: false,
+        selected: false
+    },
+    {
+        index: 4,
+        page: "Instructions",
+        highlight: false,
+        selected: false
+    },
+    {
+        index: 5,
+        page: "Materials",
+        highlight: false,
+        selected: false
+    },
+    {
+        index: 6,
+        page: "Contact",
+        highlight: true,
+        selected: false
+    }];
+
+    $rootScope.$on("page:beforeMove", function (event, args) {
+        self.navigated = args > 1;
+        
+        angular.forEach(self.navigators, function(nav) {
+            nav.selected = nav.index == args;
+        });
+    });
+
+    self.moveTo = function (nav) {
+        $rootScope.$broadcast("page:moveTo", nav.index);        
+    };
+}]).controller('PageController', ['$rootScope', '$scope', '$timeout', function ($rootScope, $scope, $timeout) {
+    $scope.selectedPage = 1;
+
+    $scope.beforeMove = function (index) {
+        $timeout(function () {
+            $rootScope.$broadcast("page:beforeMove", index);
+        });
+    };
+
+    $scope.afterMove = function (index) {};
+
+    $rootScope.$on("page:moveTo", function (event, args) {
+        $scope.selectedPage = args;
+    });
+}]).directive('wrapper', ['$rootScope', function ($rootScope) {
+    return {
+        restrict: 'C',
+        controller: 'PageController',
+        controllerAs: 'pCtrl',
+        scope: {
+            beforeMove: '&',
+            afterMove: '&',
+        },
+        link: function (scope, element, attrs) {
+            scope.$watch("selectedPage", function (a, b) {
+                if (a != b) {
+                    $(element).moveTo(a);
+                }
+            });
+
+            $(element).onepage_scroll({
+                sectionContainer: "section",
+                loop: false,
+                updateURL: false,
+                beforeMove: scope.beforeMove,
+                afterMove: scope.afterMove,
+            });
+        }
+    };
+}]);
