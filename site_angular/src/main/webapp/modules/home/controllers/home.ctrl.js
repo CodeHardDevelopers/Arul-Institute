e0b89af9f224e14bb07fd020282c765dd4e06d1a
KalyanSriram <kalyanthedeveloper@gmail.com>
2015-03-24 02:22:52 +0530

    Merged with the latest css changes

--- site_angular/src/main/webapp/modules/home/controllers/home.ctrl.js
+++ site_angular/src/main/webapp/modules/home/controllers/home.ctrl.js
@@ -29,33 +29,7 @@ function HomeCtrl ($scope, $state, $stateParams, $rootScope) {
     //  SCOPE FUNCTIONS
     //####################################
 
-    $scope.scrollToPageIfNeeded = scrollToPageIfNeeded;
-
-    var slides = $scope.slides = [];
-
-
-
-    slides.push({
-      image: 'images/classroom.jpg',
-      text: '@rul institute'
-    });
-
-    slides.push({
-      image: 'images/abdulkalam.jpg',
-      text: 'Confidence & hard work is the best medicine to kill the disease called failure'
-    });
-
-    slides.push({
-      image: 'images/einstein.jpg',
-      text: 'Education is not the learning of facts, but the training of the mind to think'
-    });
 
     console.log('HomeCtrl: came inside home controller');
-
-
-    function scrollToPageIfNeeded(){
-        console.log("came inside scroll to page");
-        $.fn.fullpage.moveTo(2);
-    }
 
 }
